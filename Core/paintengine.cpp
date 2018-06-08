77d09aef4b463a7c22136a77532f7544aee512b8
Yihong <1072042276@qq.com>
2018-06-08 19:09:40 +0800

    feat(XPaintEngine):实现了直接调用Qt绘图API的绘图引擎和自实现的绘图引擎

--- Core/paintengine.cpp
+++ Core/paintengine.cpp
@@ -1,6 +1,131 @@
-#include "Corepaintengine.h"
+#include "paintengine.h"
 
 PaintEngine::PaintEngine(QObject *parent) : QObject(parent)
 {
 
+}
+
+void PaintEngine::drawPoint(const QPointF &point, QPainter &painter)
+{
+    drawPoint(point.x(),point.y(),painter);
+}
+
+void PaintEngine::drawPoints(const PaintEngine::PointSet &ps, QPainter &painter)
+{
+    foreach (QPointF p, ps)
+    {
+        drawPoint(p,painter);
+    }
+}
+
+void PaintEngine::drawLine(const QPointF &p1, const QPointF &p2, QPainter &painter)
+{
+    drawLine(p1.x(),p1.y(),p2.x(),p2.y(),painter);
+}
+
+void PaintEngine::drawLine(const QLineF& line, QPainter &painter)
+{
+    drawLine(line.p1(),line.p2(),painter);
+}
+
+void PaintEngine::drawLines(const PaintEngine::LineSet &ls, QPainter &painter)
+{
+    foreach (QLineF l, ls)
+    {
+        drawLine(l,painter);
+    }
+}
+
+
+QtPaintEngine::QtPaintEngine(QObject *parent):
+    PaintEngine(parent)
+{
+
+}
+
+void QtPaintEngine::drawPoint(qreal x, qreal y, QPainter &painter)
+{
+    painter.drawPoint(x,y);
+}
+
+void QtPaintEngine::drawPoint(const QPointF &point, QPainter &painter)
+{
+    painter.drawPoint(point);
+}
+
+void QtPaintEngine::drawPoints(const PaintEngine::PointSet &ps, QPainter &painter)
+{
+    painter.drawPoints(ps);
+}
+
+void QtPaintEngine::drawLine(qreal x1, qreal y1, qreal x2, qreal y2, QPainter &painter)
+{
+    painter.drawLine(x1,y1,x2,y2);
+}
+
+void QtPaintEngine::drawLine(const QPointF &p1, const QPointF &p2, QPainter &painter)
+{
+    painter.drawLine(p1,p2);
+}
+
+void QtPaintEngine::drawLine(const QLineF& line, QPainter &painter)
+{
+    painter.drawLine(line);
+}
+
+void QtPaintEngine::drawLines(const PaintEngine::LineSet &ls, QPainter &painter)
+{
+    painter.drawLines(ls);
+}
+
+
+
+MCadPaintEngine::MCadPaintEngine(QObject *parent):
+    PaintEngine(parent)
+{
+
+}
+
+void MCadPaintEngine::drawPoint(qreal x, qreal y, QPainter &painter)
+{
+    painter.drawPoint(x,y);
+}
+
+void MCadPaintEngine::drawLine(qreal x1, qreal y1, qreal x2, qreal y2, QPainter &painter)
+{
+    //Bresenham 直线生成算法
+    qreal dx = x2 - x1;
+    qreal dy = y2 - y1;
+    qreal ux = ((dx > 0) << 1) - 1;//x的增量方向，取或-1
+    qreal uy = ((dy > 0) << 1) - 1;//y的增量方向，取或-1
+    qreal x = x1, y = y1;
+    int eps=0;//eps为累加误差
+
+    dx = qAbs(dx);
+    dy = qAbs(dy);
+
+    if (dx > dy)
+    {
+        for (x = x1; x != x2; x += ux)
+        {
+             drawPoint(x,y,painter);
+             eps += dy;
+             if ((eps << 1) >= dx)
+             {
+                  y += uy; eps -= dx;
+             }
+        }
+    }
+    else
+    {
+        for (y = y1; y != y2; y += uy)
+        {
+             drawPoint(x,y,painter);
+             eps += dx;
+             if ((eps << 1) >= dy)
+             {
+                  x += ux; eps -= dy;
+             }
+        }
+    }
 }
--- Core/paintengine.h
+++ Core/paintengine.h
@@ -2,16 +2,68 @@
 #define COREPAINTENGINE_H
 
 #include <QObject>
+#include <QPainter>
 
+/**
+ * @brief 绘制引擎基类
+ */
 class PaintEngine : public QObject
 {
     Q_OBJECT
 public:
+    typedef QVector<QPointF> PointSet;
+    typedef QVector<QLineF> LineSet;
+
     explicit PaintEngine(QObject *parent = 0);
+
+    virtual void drawPoint(qreal x, qreal y, QPainter& painter) = 0;
+    virtual void drawPoint(const QPointF& point,QPainter& painter);
+    virtual void drawPoints(const PointSet& ps, QPainter& painter);
+
+    virtual void drawLine(qreal x1, qreal y1, qreal x2, qreal y2, QPainter& painter) = 0;
+    virtual void drawLine(const QPointF& p1, const QPointF& p2, QPainter& painter);
+    virtual void drawLine(const QLineF& line, QPainter& painter);
+    virtual void drawLines(const LineSet& ls, QPainter& painter);
+
+//    virtual void drawRect(const QRectF &rectangle);
+//    virtual void drawEllipse(const QPointF &center, qreal rx, qreal ry);
 
 signals:
 
 public slots:
+};
+
+/**
+ * @brief 使用Qt GUI内嵌的图形绘制API，CPU绘图,也可使用OpenGL调用GPU绘图
+ */
+class QtPaintEngine : public PaintEngine
+{
+public:
+
+    explicit QtPaintEngine(QObject* parent = 0);
+
+    virtual void drawPoint(qreal x, qreal y, QPainter& painter);
+    virtual void drawPoint(const QPointF& point,QPainter& painter);
+    virtual void drawPoints(const PointSet& ps, QPainter& painter);
+
+    virtual void drawLine(qreal x1, qreal y1, qreal x2, qreal y2, QPainter& painter);
+    virtual void drawLine(const QPointF& p1, const QPointF& p2, QPainter& painter);
+    virtual void drawLine(const QLineF &line, QPainter& painter);
+    virtual void drawLines(const LineSet& ls, QPainter& painter);
+
+};
+
+/**
+ * @brief 只使用Qt图形绘制API的画点API，CPU绘制
+ */
+class MCadPaintEngine : public PaintEngine
+{
+public:
+    explicit MCadPaintEngine(QObject* parent = 0);
+
+    virtual void drawPoint(qreal x, qreal y, QPainter& painter);
+    virtual void drawLine(qreal x1, qreal y1, qreal x2, qreal y2, QPainter& painter);
+
 };
 
 #endif // COREPAINTENGINE_H
