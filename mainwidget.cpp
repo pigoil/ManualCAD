7fa69915df342c0563b045e44cf4d9fda52e5269
Yihong <1072042276@qq.com>
2018-06-08 19:11:21 +0800

    test(MainWindow)

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,6 +1,37 @@
 #include "mainwidget.h"
+#include <QPaintEvent>
 
 MCadWidget::MCadWidget(QWidget *parent) : QWidget(parent)
 {
+    m_paint_engine = new MCadPaintEngine(this);
+    m_command = new MCadCommand::PlaceCircle(this);
 
+    setMouseTracking(true);
+}
+
+void MCadWidget::paintEvent(QPaintEvent *e)
+{
+    QPainter p;
+    p.begin(this);
+    p.fillRect(e->rect(),Qt::black);
+    p.setPen(Qt::green);
+    m_command->paint(e,p);
+    p.drawText(0,20,m_command->hint());
+    //m_paint_engine->drawLine(0,0,20,20,p);
+}
+
+void MCadWidget::mouseMoveEvent(QMouseEvent *e)
+{
+    m_command->move(e);
+    update();
+}
+
+void MCadWidget::mousePressEvent(QMouseEvent *e)
+{
+    if(e->button() == Qt::LeftButton)
+    {
+        m_command->proceed(e);
+        update();
+    }
+    QWidget::mousePressEvent(e);
 }
--- mainwidget.h
+++ mainwidget.h
@@ -2,12 +2,24 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include "paintengine.h"
+#include "usercommand.h"
 
 class MCadWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit MCadWidget(QWidget *parent = 0);
+
+protected:
+    virtual void paintEvent(QPaintEvent *e);
+    virtual void mouseMoveEvent(QMouseEvent* e);
+    virtual void mousePressEvent(QMouseEvent* e);
+    //virtual bool event(QEvent *event){}
+
+private:
+    PaintEngine* m_paint_engine;
+    UserCommand* m_command;
 
 signals:
 
